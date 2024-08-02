8c56379aab6b1bb2f365fc37e487466a927ade6e
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-08-02 03:45:35 -0400

    add a new allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
     'http://localhost:3002',
     'http://localhost:3500',
     // 'https://www.dandrepairshop.com',
-    'https://technotes-zbnt.onrender.com'
+    'https://technotes-zbnt.onrender.com',
+    'https://mawuhi-back.onrender.com'
 
 ]
 
