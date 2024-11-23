26cbda22045d8cb7c59899f57f2c91003250168b
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-11-23 16:14:15 -0500

    added an allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://localhost:3001',
     'https://mawuhi.onrender.com',
     // 'https://prim3timer.github.io/Retailer',
-    'https://prim3timer.github.io/test/results'
+    'https://prim3timer.github.io'
 
 ]
 
