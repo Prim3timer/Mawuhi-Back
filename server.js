37be8e6b2ef3ba4046852effdf751eff064298f7
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-07-02 12:19:45 -0400

    finding the route

--- server.js
+++ server.js
@@ -42,14 +42,15 @@ app.use(verifyJWT);
 
 
     app.all('*', (req, res)=> { 
-        res.status(404)
-        if (req.accepts('html')){
-            res.sendFile(path.join(__dirname, 'views', '404.html'))
-        } else if (req.accepts('json')){
-            res.json({message: '404 not found'})
-        }else {
-            res.type('txt').send('404 not found')
-        }
+        res.sendFile(path.join(__dirname, 'views', 'index.html'))
+        // res.status(404)
+        // if (req.accepts('html')){
+        //     res.sendFile(path.join(__dirname, 'views', '404.html'))
+        // } else if (req.accepts('json')){
+        //     res.json({message: '404 not found'})
+        // }else {
+        //     res.type('txt').send('404 not found')
+        // }
     })
 
 
