0af8f65e0b2287188b12ff5874856adca3602fa2
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-07-02 21:45:36 -0400

    routing issur

--- server.js
+++ server.js
@@ -41,16 +41,15 @@ app.use('/create-checkout-session', require('./routes/cartRoutes'))
 app.use(verifyJWT);
 
 
-    app.all('*', (req, res)=> { 
-        res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    app.all('/*', (req, res)=> { 
         // res.status(404)
-        // if (req.accepts('html')){
-        //     res.sendFile(path.join(__dirname, 'views', '404.html'))
-        // } else if (req.accepts('json')){
-        //     res.json({message: '404 not found'})
-        // }else {
-        //     res.type('txt').send('404 not found')
-        // }
+        if (req.accepts('html')){
+            res.sendFile(path.join(__dirname, 'views', '404.html'))
+        } else if (req.accepts('json')){
+            res.json({message: '404 not found'})
+        }else {
+            res.type('txt').send('404 not found')
+        }
     })
 
 
