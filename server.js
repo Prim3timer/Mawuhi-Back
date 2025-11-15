286eef7c78335ec4a36bd1596e422028090c2157
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2025-11-15 17:50:43 -0500

    little tweek

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ app.use('/refresh', require('./routes/refreshRoutes'))
 // app.use('/create-checkout-session', require('./routes/cartRoutes'))
 
 app.use('/transactions', require('./routes/transactionRoutes'))
+app.use('/items', require('./routes/itemRoutes'))   
 app.use(verifyJWT); 
 app.use('/users', require('./routes/userRoutes'))   
-app.use('/items', require('./routes/itemRoutes'))   
 
 
     app.all('/*', (req, res)=> { 
